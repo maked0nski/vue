071dd05d810c6849e0ad6248a54ebbad8e1baa76
maked0nski <maked0nskihtc@gmail.com>
2020-08-30 23:18:14 +0200

    додав опис роботи з основними плагінами в ReadMe.MD

--- vue_store_learn/src/main.js
+++ vue_store_learn/src/main.js
@@ -1,8 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueResource from 'VueResource'
+
+import 'bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 Vue.config.productionTip = false
+Vue.use(VueResource)
 
 new Vue({
+  VueResource,
   render: h => h(App),
 }).$mount('#app')
